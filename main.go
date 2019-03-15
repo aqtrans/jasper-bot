ffe8ac1b12987be327312b3faa1cd165a6a674cc
Jordan Anderson <me@jba.io>
2019-03-15 17:49:56 -0500

    Bot now listens and joins rooms dynamically. No more hardcoded room Closes issue #2

--- main.go
+++ main.go
@@ -30,13 +30,15 @@ func main() {
 	if !cfgTree.Has("pass") {
 		log.Fatalln(*confFile, "does not have pass defined.")
 	}
-	if !cfgTree.Has("room") {
-		log.Fatalln(*confFile, "does not have room defined.")
-	}
+	/*
+		if !cfgTree.Has("room") {
+			log.Fatalln(*confFile, "does not have room defined.")
+		}
+	*/
 	homeserver := cfgTree.Get("homeserver").(string)
 	user := cfgTree.Get("user").(string)
 	pass := cfgTree.Get("pass").(string)
-	room := cfgTree.Get("room").(string)
+	//room := cfgTree.Get("room").(string)
 
 	cli, err := gomatrix.NewClient(homeserver, "", "")
 	if err != nil {
@@ -54,10 +56,6 @@ func main() {
 
 	cli.SetCredentials(login.UserID, login.AccessToken)
 
-	if _, err := cli.JoinRoom(room, "", nil); err != nil {
-		log.Fatalln("JoinRoom error:", err)
-	}
-
 	/*
 		sendEvent, err := cli.SendText(room, "OMG!")
 		if err != nil {
@@ -66,9 +64,26 @@ func main() {
 		log.Println(sendEvent.EventID)
 	*/
 
-	log.Println("Now connected and listening for '!jasper' in specified room!")
+	log.Println("Now connected. Invite", login.UserID, "to any rooms and start sending it messages starting with !jasper.")
 
 	syncer := cli.Syncer.(*gomatrix.DefaultSyncer)
+	// Listen for room invites:
+	syncer.OnEventType("m.room.member", func(ev *gomatrix.Event) {
+		if *ev.StateKey != login.UserID {
+			return // not our member event
+		}
+		m := ev.Content["membership"]
+		membership, ok := m.(string)
+		if !ok {
+			return
+		}
+		if membership == "invite" {
+			log.Println("Invite received:", ev.Sender, ev)
+			if _, err := cli.JoinRoom(ev.RoomID, "", nil); err != nil {
+				log.Fatalln("JoinRoom error:", err)
+			}
+		}
+	})
 	syncer.OnEventType("m.room.message", func(ev *gomatrix.Event) {
 		msg, ok := ev.Body()
 		if ok {
@@ -79,7 +94,7 @@ func main() {
 				// Grab image from thatsapaddl.in
 				tap, err := http.Get("https://thatsapaddl.in/" + memeText)
 				if err != nil {
-					log.Fatalln("Error getting meme.", err)
+					log.Fatalln("Error getting meme:", err)
 				}
 				tapImage := new(bytes.Buffer)
 				_, err = io.Copy(tapImage, tap.Body)
@@ -99,11 +114,10 @@ func main() {
 					log.Fatalln("Error uploading meme:", err)
 				}
 
-				imageEvent, err := cli.SendImage(room, "That's a Paddlin'", upload.ContentURI)
+				_, err = cli.SendImage(ev.RoomID, "That's a Paddlin'", upload.ContentURI)
 				if err != nil {
 					log.Fatalln("SendImage error:", err)
 				}
-				log.Println(imageEvent.EventID)
 			}
 		}
 	})
